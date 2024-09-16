cf08588e6509b608056630f867d831180804051e
Niklas Trapp <niklas031712@gmail.com>
2024-09-16 11:26:03 +0200

    finish task 3

--- number_ninja.rb
+++ number_ninja.rb
@@ -1,34 +1,47 @@
 class NumberNinja
-    def initialize
-      @secret_number = rand(0..100)
-      @tries = 0
-    end
-  
     def play
-      puts "Willkommen bei NumberNinja! Errate die Zahl zwischen 0 und 100."
-      loop do
-        print "Gib deine Vermutung ein: "
-        guess = gets.chomp.to_i
+        @tries = 0
+        puts "Willkommen bei NumberNinja! Wähle bitte zuerst einen Schwierigkeitsgrad aus."
+        puts "leicht: 0-50, mittel: 0-100, schwer: 0-200"
 
-        # Validation
-        if guess > 100 or guess < 0
-            puts "Fehlerhafte Eingabe. Bitte erneut versuchen"
-        else
-            @tries += 1
-
-            if guess == @secret_number
-                puts "Glückwunsch! Du hast die Zahl in #{@tries} Versuchen erraten!"
-                break
-              elsif guess < @secret_number
-                puts "Zu niedrig! Versuche es nochmal."
-              else
-                puts "Zu hoch! Versuche es nochmal."
-              end
+        difficulty = gets.chomp
+        
+        case difficulty
+        when "leicht"
+            @max = 50
+        when "mittel"
+            @max = 100
+        when "schwer"
+            @max = 200
         end
-      end
+
+        @secret_number = rand(0...@max.to_i)
+
+        puts "Errate nun die Zahl zwischen 0 und #{@max}."
+
+        loop do
+            print "Gib deine Vermutung ein: "
+            guess = gets.chomp.to_i
+
+            # Validation
+            if guess > @max or guess < 0
+                puts "Fehlerhafte Eingabe. Bitte erneut versuchen"
+            else
+                @tries += 1
+
+                if guess == @secret_number
+                    puts "Glückwunsch! Du hast die Zahl in #{@tries} Versuchen erraten!"
+                    break
+                elsif guess < @secret_number
+                    puts "Zu niedrig! Versuche es nochmal."
+                else
+                    puts "Zu hoch! Versuche es nochmal."
+                end
+            end
+        end
     end
-  end
+end
   
-  game = NumberNinja.new
-  game.play
+game = NumberNinja.new
+game.play
   
